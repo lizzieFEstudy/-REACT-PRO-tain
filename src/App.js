ecec970aeb39275e656c278575c49194579e078e
TerryEHLee <terryehlee@gmail.com>
2023-07-17 19:27:36 +0900

    start files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './App.css';
+import styled from 'styled-components';
+import uuid from 'react-uuid';
+
+function App() {
+  return <></>;
+}
+
+export default App;
