2bd7de965dc9cfb97e790e39150494777d649ca9
podoDJ <ourstar1345@gmail.com>
2023-07-18 09:39:38 +0900

    feature/login-register-temp for project

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom/dist";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Details from "../pages/Details";
 
-function Router() {
-  return <div>Router</div>;
-}
+
+const Router = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      // 임시로 true값으로 설정.
+      setIsLoggedIn(false);
+    }
+  }, [isLoggedIn]);
+  
+  return (
+    <BrowserRouter>
+      {isLoggedIn ? (
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/:id" element={<Details />} />
+          <Route path="/login" element={<Navigate to="/"/>} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      )}
+    </BrowserRouter>
+  );
+};
 
 export default Router;
