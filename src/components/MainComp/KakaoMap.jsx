b99f6d42e81db323411e485d6cb1c5649f7b9cbd
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-07-18 13:42:00 +0900

    feat(KakaoMap): 지도 연결

---
+++ src/components/MainComp/KakaoMap.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+const { kakao } = window;
+
+const KakaoMap = () => {
+  useEffect(() => {
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  }, []);
+
+  return (
+    <div className="map_wrap">
+      <div id="map" style={{ width: '100vw', height: '100vh', position: 'relative', overflow: 'hidden' }}></div>
+      <div id="menu_wrap">
+        <ul id="placesList"></ul>
+        <div id="pagination"></div>
+      </div>
+    </div>
+  );
+};
+
+export default KakaoMap;
