d25de4bc36972d2a778d08d31edc2c22f20d7823
TerryEHLee <terryehlee@gmail.com>
2023-07-20 15:19:07 +0900

    detail-read

--- src/components/DetailComp/DetailBox.jsx
+++ src/components/DetailComp/DetailBox.jsx
@@ -3,11 +3,63 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { UseSelector } from 'react-redux';
 import styled from 'styled-components';
 import { getComments, addComment, deleteComment, updateComment } from '../../api/comments';
+import { useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { auth } from '../../firebase';
 
 function DetailBox() {
   const navigate = useNavigate();
 
   const params = useParams();
+
+  const [nickName, SetNickName] = useState();
+  const [comment, SetComment] = useState();
+  const { isLoading, isError, data } = useQuery('comments', getComments);
+  const { id } = params;
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(addComment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('comments');
+    }
+  });
+
+  const deleteMutation = useMutation(deleteComment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('comments');
+    }
+  });
+
+  const addCommentHandler = async (e) => {
+    e.preventDefault();
+
+    if (comment) {
+      const newComment = {
+        // id: id,
+        nickName,
+        comment,
+        userId: auth.currentUser.uid
+      };
+
+      mutation.mutate(newComment);
+
+      SetNickName('');
+      SetComment('');
+    } else {
+      alert('모든 항목을 입력하세요');
+    }
+  };
+
+  const deleteCommentHandler = (id) => {
+    const confirmed = window.confirm('이 댓글을 삭제하시겠습니까?');
+    if (confirmed) {
+      deleteMutation.mutate(id);
+    }
+  };
+
+  const commentHandler = (e) => {
+    SetComment(e.target.value);
+  };
 
   return (
     <>
@@ -18,6 +70,9 @@ function DetailBox() {
 
       <SReviewBox>
         <h1>Reviews..</h1>
+        <br />
+        <CommentInput type="text" value={comment} onChange={commentHandler} placeholder="내용을 입력하세요." />
+        <button onClick={addCommentHandler}>등록</button>
         <br />
         <div>name| 별점 7.8| 22.04.05</div>
         <button>수정</button>
@@ -38,4 +93,15 @@ const SInfoBox = styled.div`
 const SReviewBox = styled.div`
   flex: 1;
   margin: 30px;
+`;
+
+const CommentInput = styled.input`
+  background: transparent;
+  border: 1px solid white;
+  margin-left: 20px;
+  margin-bottom: 20px;
+  width: 500px;
+  height: 30px;
+  padding: 5px;
+  color: white;
 `;
