db9c52d3bade0764f9cdfe30eed6440a6a020792
podoDJ <ourstar1345@gmail.com>
2023-07-21 17:15:49 +0900

    temp

--- src/components/DetailComp/DetailBox.jsx
+++ src/components/DetailComp/DetailBox.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { UseSelector } from 'react-redux';
-import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
 import { getComments, addComment, deleteComment, updateComment } from '../../api/comments';
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { auth } from '../../firebase';
+import { addShops, getShops, updateShops } from '../../api/shops';
 
-function DetailBox() {
+function DetailBox( { placeData }) {
   const navigate = useNavigate();
 
   const params = useParams();
-  const { placeData } = useParams();
 
-  const [nickName, setNickName] = useState("");
-  const [comment, setComment] = useState("");
+  const [nickName, setNickName] = useState('');
+  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState(0);
+
   const { isLoading, isError, data } = useQuery('comments', getComments);
-  const shopId = params.id; 
-  console.log("shopId=>", shopId)
+  console.log("data=>",data?.map((item) => item.id))
+  const shopId = params.id;
+  //shop등록용
+  const { isLoading: shopLoading, isError: shopError, data: shopData } = useQuery('shops', getShops);
+
   const queryClient = useQueryClient();
   const mutation = useMutation(addComment, {
     onSuccess: () => {
       queryClient.invalidateQueries('comments');
+    },
+    onError: () => {
+      alert("애드코멘트 오류")
     }
   });
 
@@ -36,25 +44,51 @@ function DetailBox() {
       queryClient.invalidateQueries('comments');
     }
   });
+  /////shop 등록용 mutation-동준-/////
+  const addShopMutation = useMutation(addShops, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('shops');
+      alert('신규 shop 등록 성공');
+    },
+    onError: () => {
+      alert('신규 shop 등록 에러');
+    }
+  });
+
+  const updateShopMutation = useMutation(updateShops, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('shops');
+      alert('shop 업데이트 성공');
+    },
+    onError: () => {
+      alert('shop 업데이트 에러');
+    }
+  });
+
+  /////shop 등록용 mutation-동준-////
+  console.log("shopData=>",shopData?.map((item)=> item.id))
 
   const addCommentHandler = async (e) => {
     e.preventDefault();
 
-    if (comment) {
-      const newComment = {
-        shopId,
-        nickName,
-        comment,
-        userId: auth.currentUser.uid
-      };
-
-      mutation.mutate(newComment);
-
-      setNickName('');
-      setComment('');
-    } else {
+    if (!comment || rating === 0) {
       alert('모든 항목을 입력하세요');
+      return;
     }
+
+    const newComment = {
+      shopId,
+      nickName,
+      comment,
+      rating, // Save the selected rating in the new comment object
+      userId: auth.currentUser.uid
+    };
+
+    mutation.mutate(newComment);
+
+    setNickName('');
+    setComment('');
+    setRating(0); // Reset rating after adding the comment
   };
 
   const deleteCommentHandler = (id) => {
@@ -75,47 +109,102 @@ function DetailBox() {
     setComment(event.target.value);
   };
 
+  const handleRatingSelection = (ratingValue) => {
+    setRating(ratingValue);
+  };
+
+  const addShopHandler = async () => {
+    if (!shopLoading) {
+      const shopExists = shopData?.some((item) => item.id === shopId);
+      if (!shopExists) {
+        const newShop = {
+          id: shopId,
+          comment: [Number(rate)]
+        };
+        addShopMutation.mutate(newShop);
+      } else {
+        const shopToUpdate = shopData.find((item) => item.id === shopId);
+        const updatedShop = {
+          ...shopToUpdate,
+          comment: [...shopToUpdate.comment, Number(rate)]
+        };
+        updateShopMutation.mutate({ shopId, updatedShop });
+      }
+    }
+  };
+
   return (
     <>
-      {placeData && (
-        <SInfoBox>
-          <h1>{placeData.place_name}</h1>
-          <div>⭐️ 7.5/10 방문자 리뷰 300</div>
-        </SInfoBox>
-      )}
-
-      <SReviewBox>
-        <h1>Reviews..</h1>
-        <br />
-        <CommentInput type="text" value={comment} onChange={(event) => commentHandler(event)} placeholder="내용을 입력하세요." />
-        <button onClick={addCommentHandler}>등록</button>
-        <br />
-        {data
-          ?.filter((comment) => comment.shopId == shopId)
-          .map((comment) => {
-            return (
-              <div key={comment.id}>
-                {/* <div>{users.name}</div> */}
-                <div>name| 별점 7.8| 22.04.05</div>
-                <button
-                  onClick={() => {
-                    updateCommentHandler(comment.id);
-                  }}
-                >
-                  수정
-                </button>
-                <button
-                  onClick={() => {
-                    deleteCommentHandler(comment.id);
-                  }}
-                >
-                  삭제
-                </button>
-                <div>{comment.comment}</div>
-              </div>
-            );
-          })}
-      </SReviewBox>
+      <StDetailPage style={{ marginTop: '100px' }}>
+        <StDetailBox size="placeTitle">
+          <div>{placeData?.place_name}</div>
+          <StReviewCountBox>
+            <div>별점자리</div>
+            <div>방문자 리뷰</div>
+          </StReviewCountBox>
+        </StDetailBox>
+        <StDetailBox size="placeDetail">
+          <div>{placeData?.road_address_name}</div>
+          <div>{placeData?.phone}</div>
+        </StDetailBox>
+        <StDetailBox size="placeReviews">
+          <br />
+          {data
+            ?.filter((comment) => comment.shopId == shopId)
+            .map((comment) => {
+              return (
+                <div key={comment.id}>
+                  {/* <div>{users.name}</div> */}
+                  <div>name| 별점 7.8| 22.04.05</div>
+                  <button
+                    onClick={() => {
+                      updateCommentHandler(comment.id);
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteCommentHandler(comment.id);
+                    }}
+                  >
+                    삭제
+                  </button>
+                  <div>{comment.comment}</div>
+                </div>
+              );
+            })}
+        </StDetailBox>
+        <StDetailBox size="placeDetail">
+          <h1>리뷰를 남겨보세요</h1>
+          <br />
+          <div>
+            {/* Star rating selection buttons */}
+            <StarButton active={rating >= 1} onClick={() => handleRatingSelection(1)}>
+              ★
+            </StarButton>
+            <StarButton active={rating >= 2} onClick={() => handleRatingSelection(2)}>
+              ★
+            </StarButton>
+            <StarButton active={rating >= 3} onClick={() => handleRatingSelection(3)}>
+              ★
+            </StarButton>
+            <StarButton active={rating >= 4} onClick={() => handleRatingSelection(4)}>
+              ★
+            </StarButton>
+            <StarButton active={rating >= 5} onClick={() => handleRatingSelection(5)}>
+              ★
+            </StarButton>
+          </div>
+          <CommentInput
+            type="text"
+            value={comment}
+            onChange={(event) => commentHandler(event)}
+            placeholder="내용을 입력하세요."
+          />
+          <button onClick={addCommentHandler}>등록</button>
+        </StDetailBox>
+      </StDetailPage>
     </>
   );
 }
@@ -137,8 +226,60 @@ const CommentInput = styled.input`
   border: 1px solid white;
   margin-left: 20px;
   margin-bottom: 20px;
-  width: 500px;
+  width: 300px;
   height: 30px;
   padding: 5px;
   color: black;
+`;
+
+const StDetailPage = styled.div`
+  margin: 100px auto 0px;
+  border: 1px solid red;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StDetailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${({ size }) => {
+    switch (size) {
+      case 'placeTitle':
+        return css`
+          width: 50%;
+          height: 20%;
+        `;
+      case 'placeDetail':
+        return css`
+          width: 70%;
+          height: 20%;
+        `;
+      case 'placeReviews':
+        return css`
+          width: 70%;
+          height: 80%;
+        `;
+    }
+  }}
+  border: 1px solid black;
+
+  /* align-items: center; */
+`;
+const StReviewCountBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StarButton = styled.button`
+  font-size: 20px;
+  color: ${(props) => (props.active ? 'gold' : 'gray')};
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
 `;
