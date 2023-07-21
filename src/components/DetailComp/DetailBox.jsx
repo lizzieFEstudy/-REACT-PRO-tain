8c9f33a342743958db98d98420df06c5c2498202
TerryEHLee <terryehlee@gmail.com>
2023-07-21 14:04:28 +0900

    components 합(DetailBox.jsx+DetailInfo.jsx)

--- src/components/DetailComp/DetailBox.jsx
+++ src/components/DetailComp/DetailBox.jsx
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { UseSelector } from 'react-redux';
-import styled from 'styled-components';
+import { css, styled } from 'styled-components';
 import { getComments, addComment, deleteComment, updateComment } from '../../api/comments';
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { auth } from '../../firebase';
 
-function DetailBox() {
+const DetailBox = ({ placeData }) => {
   const navigate = useNavigate();
 
   const params = useParams();
-  const { placeData } = useParams();
 
-  const [nickName, setNickName] = useState("");
-  const [comment, setComment] = useState("");
+  const [nickName, setNickName] = useState('');
+  const [comment, setComment] = useState('');
   const { isLoading, isError, data } = useQuery('comments', getComments);
-  const shopId = params.id; 
-  console.log("shopId=>", shopId)
+  const shopId = params.id;
+  console.log('shopId=>', shopId);
   const queryClient = useQueryClient();
   const mutation = useMutation(addComment, {
     onSuccess: () => {
@@ -77,48 +76,59 @@ function DetailBox() {
 
   return (
     <>
-      {placeData && (
-        <SInfoBox>
-          <h1>{placeData.place_name}</h1>
-          <div>⭐️ 7.5/10 방문자 리뷰 300</div>
-        </SInfoBox>
-      )}
-
-      <SReviewBox>
-        <h1>Reviews..</h1>
-        <br />
-        <CommentInput type="text" value={comment} onChange={(event) => commentHandler(event)} placeholder="내용을 입력하세요." />
-        <button onClick={addCommentHandler}>등록</button>
-        <br />
-        {data
-          ?.filter((comment) => comment.shopId == shopId)
-          .map((comment) => {
-            return (
-              <div key={comment.id}>
-                {/* <div>{users.name}</div> */}
-                <div>name| 별점 7.8| 22.04.05</div>
-                <button
-                  onClick={() => {
-                    updateCommentHandler(comment.id);
-                  }}
-                >
-                  수정
-                </button>
-                <button
-                  onClick={() => {
-                    deleteCommentHandler(comment.id);
-                  }}
-                >
-                  삭제
-                </button>
-                <div>{comment.comment}</div>
-              </div>
-            );
-          })}
-      </SReviewBox>
+      <StDetailPage style={{ marginTop: '100px' }}>
+        <StDetailBox size="placeTitle">
+          <div>{placeData?.place_name}</div>
+          <StReviewCountBox>
+            <div>별점자리</div>
+            <div>방문자 리뷰</div>
+          </StReviewCountBox>
+        </StDetailBox>
+        <StDetailBox size="placeDetail">
+          <div>{placeData?.road_address_name}</div>
+          <div>{placeData?.phone}</div>
+        </StDetailBox>
+        <StDetailBox size="placeReviews">
+          <h1>Reviews..</h1>
+          <br />
+          <CommentInput
+            type="text"
+            value={comment}
+            onChange={(event) => commentHandler(event)}
+            placeholder="내용을 입력하세요."
+          />
+          <button onClick={addCommentHandler}>등록</button>
+          <br />
+          {data
+            ?.filter((comment) => comment.shopId == shopId)
+            .map((comment) => {
+              return (
+                <div key={comment.id}>
+                  {/* <div>{users.name}</div> */}
+                  <div>name| 별점 7.8| 22.04.05</div>
+                  <button
+                    onClick={() => {
+                      updateCommentHandler(comment.id);
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteCommentHandler(comment.id);
+                    }}
+                  >
+                    삭제
+                  </button>
+                  <div>{comment.comment}</div>
+                </div>
+              );
+            })}
+        </StDetailBox>
+      </StDetailPage>
     </>
   );
-}
+};
 
 export default DetailBox;
 
@@ -141,4 +151,47 @@ const CommentInput = styled.input`
   height: 30px;
   padding: 5px;
   color: black;
+`;
+
+const StDetailPage = styled.div`
+  margin: 100px auto 0px;
+  border: 1px solid red;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StDetailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${({ size }) => {
+    switch (size) {
+      case 'placeTitle':
+        return css`
+          width: 50%;
+          height: 20%;
+        `;
+      case 'placeDetail':
+        return css`
+          width: 70%;
+          height: 20%;
+        `;
+      case 'placeReviews':
+        return css`
+          width: 70%;
+          height: 80%;
+        `;
+    }
+  }}
+  border: 1px solid black;
+
+  /* align-items: center; */
+`;
+const StReviewCountBox = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
