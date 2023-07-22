0171f60ac5138dd7e3d133741bcfda1028ef4956
podoDJ <ourstar1345@gmail.com>
2023-07-22 12:27:31 +0900

    merge not finished

--- src/components/DetailComp/DetailBox.jsx
+++ src/components/DetailComp/DetailBox.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import styled, { css } from 'styled-components';
-=======
-import { UseSelector } from 'react-redux';
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
-import styled, { css } from 'styled-components';
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
 import { getComments, addComment, deleteComment, updateComment } from '../../api/comments';
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { auth } from '../../firebase';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import { VscTriangleDown } from 'react-icons/vsc';
-=======
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
 import { getUsers } from '../../api/users';
 import {
   CommentInput,
@@ -34,45 +21,23 @@ import {
   StBtnWrap,
   StCommentDetails
 } from './DetailStyles';
-<<<<<<< HEAD
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
 
 const DetailBox = ({ placeData }) => {
   const navigate = useNavigate();
   const params = useParams();
 
+  const [nickName, setNickName] = useState('');
   const [comment, setComment] = useState('');
   const [rating, setRating] = useState(0);
 
   const [displayedComments, setDisplayedComments] = useState([]);
 
-  // Define state variables for edit mode
-  const [editMode, setEditMode] = useState(false);
-  const [editCommentId, setEditCommentId] = useState(null);
-  const [editComment, setEditComment] = useState('');
-  const [editRating, setEditRating] = useState(0);
-
-  // Function to toggle edit mode
-  const toggleEditMode = (commentId, comment, rating) => {
-    setEditMode(!editMode);
-    setEditCommentId(commentId);
-    setEditComment(comment);
-    setEditRating(rating);
-  };
-
-  const { data } = useQuery('comments', getComments, {
+  const { isLoading, isError, data } = useQuery('comments', getComments, {
     onSuccess: (data) => {
-      console.log('Fetched data:', data);
       setDisplayedComments(data.filter((comment) => comment.shopId === shopId));
     }
   });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
   //가격정보 select창 관련
   const currentPlace = placeData.category_name.split('>').pop().trim()
   console.log("currentPlace=>",currentPlace)
@@ -107,21 +72,13 @@ const DetailBox = ({ placeData }) => {
     setPrice(addComma(event.target.value));
   };
 
+
   //가격정보 select창 관련
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
   const { data: userData } = useQuery('users', getUsers, {
     onSuccess: (userData) => {
       console.log('Fetched userData:', userData);
     }
   });
-<<<<<<< HEAD
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
-
   const shopId = params.id;
 
   //별점 구하는 곳
@@ -162,25 +119,17 @@ const DetailBox = ({ placeData }) => {
 
     const newComment = {
       shopId,
+      nickName,
       comment,
       rating,
       userId: auth.currentUser.uid,
-<<<<<<< HEAD
-<<<<<<< HEAD
       selected,
       price,
-=======
-      createdAt: new Date().toISOString()
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
-      selected,
-      price,
-      createdAt: new Date().toISOString()
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
     };
 
     mutation.mutate(newComment);
 
+    setNickName('');
     setComment('');
     setRating(0);
   };
@@ -193,29 +142,10 @@ const DetailBox = ({ placeData }) => {
     }
   };
 
-  const updateCommentHandler = async (id) => {
+  const updateCommentHandler = (id) => {
     const confirmed = window.confirm('이 댓글을 수정하시겠습니까?');
     if (confirmed) {
-      const updatedComment = {
-        id,
-        shopId,
-        comment: editComment,
-        rating: editRating,
-        userId: auth.currentUser.uid,
-        createdAt: new Date().toISOString()
-      };
-
-      try {
-        await updateComment(updatedComment);
-        // After successful update, reset the edit mode
-        setEditMode(false);
-        setEditCommentId(null);
-        setEditComment('');
-        setEditRating(0);
-      } catch (error) {
-        console.error('Error updating comment:', error);
-        // Handle error if needed
-      }
+      updateMutation.mutate(id);
     }
   };
 
@@ -226,28 +156,6 @@ const DetailBox = ({ placeData }) => {
   const handleRatingSelection = (ratingValue) => {
     setRating(ratingValue);
   };
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-=======
-  
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
-  const getUserName = (userId) => {
-    const user = userData?.find((user) => user.id === userId);
-    return user?.name || 'Unknown User'; // Return 'Unknown User' if user is not found
-  };
-
-  const formatDate = (dateString) => {
-    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date(dateString);
-    return isNaN(date) ? 'Invalid Date' : date.toLocaleDateString('ko-KR', dateOptions);
-  };
-
-<<<<<<< HEAD
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
   return (
     <>
       <StDetailPage style={{ marginTop: '100px' }}>
@@ -265,15 +173,9 @@ const DetailBox = ({ placeData }) => {
         <StDetailBox size="placeReviews">
           <br />
           {data
-            ?.filter((comment) => comment.shopId === shopId)
+            ?.filter((comment) => comment.shopId == shopId)
             .map((comment) => {
-              const formattedDate = formatDate(comment.createdAt);
-              const isEditing = editMode && editCommentId === comment.id;
               return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
                 <div key={comment.id}>
                   {/* <div>{users.name}</div> */}
                   <strong>
@@ -301,41 +203,6 @@ const DetailBox = ({ placeData }) => {
                   </button>
                   <div>{comment.comment}</div>
                 </div>
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
-                <StCommentBox key={comment.id}>
-                  <StCommentHeader>
-                    <StCommentDetails>
-                      {/* Comment details (username, rating, date) */}
-                      <strong>{getUserName(comment.userId)}</strong> | 별점 {comment.rating.toFixed(1)} |{' '}
-                      {formattedDate !== 'Invalid Date' ? formattedDate : 'No Date'}
-                    </StCommentDetails>
-                    <StBtnWrap>
-                      {isEditing ? ( // Show "완료" (Done) button in edit mode
-                        <StCommentButtons onClick={() => updateCommentHandler(comment.id)}>완료</StCommentButtons>
-                      ) : (
-                        // Show "수정" (Edit) button in non-edit mode
-                        <StCommentButtons onClick={() => toggleEditMode(comment.id, comment.comment, comment.rating)}>
-                          수정
-                        </StCommentButtons>
-                      )}
-                      <StCommentButtons
-                        onClick={() => {
-                          deleteCommentHandler(comment.id);
-                        }}
-                      >
-                        삭제
-                      </StCommentButtons>
-                    </StBtnWrap>
-                  </StCommentHeader>
-                  <StCommentContent>{isEditing ? null : comment.comment}</StCommentContent>
-                </StCommentBox>
-<<<<<<< HEAD
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
               );
             })}
         </StDetailBox>
@@ -390,7 +257,6 @@ const DetailBox = ({ placeData }) => {
             onChange={(event) => commentHandler(event)}
             placeholder="내용을 입력하세요."
           />
-
           <button onClick={addCommentHandler}>등록</button>
         </StDetailBox>
       </StDetailPage>
@@ -399,11 +265,6 @@ const DetailBox = ({ placeData }) => {
 };
 
 export default DetailBox;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
 
 const CommentInput = styled.input`
   background: transparent;
@@ -510,9 +371,3 @@ const StDropdownItem = styled.div`
     background-color: #f4f4f4;
   }
 `;
-<<<<<<< HEAD
-=======
->>>>>>> eaef9156f34a3d75cbc39e297cf7e70f49b93efb
-=======
-
->>>>>>> 7bdf05fbc12915a125d8fd8ef30a23c37ce546b0
